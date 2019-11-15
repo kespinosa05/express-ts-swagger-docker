847788e01897be1ad74f0e81352376c219af7a01
Kenny Lopez <klopez@ec.krugercorp.com>
2019-11-15 10:41:55 -0500

    integrate swagger ui

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,27 @@
 import * as express from 'express';
+import * as bodyParser from 'body-parser';
+import * as methodOverride from 'method-override';
+
 import { RegisterRoutes } from './routes/routes';  // here
 
 const app = express();
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use(methodOverride());
+
 const port = 3000;
+
+const swaggerUi = require('swagger-ui-express');
+try {
+    var swaggerDocument = require('../api/dist/swagger.json');
+    app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+} catch (err) {
+    console.log('Unable to load swagger.json', err);
+}
+
 
 RegisterRoutes(app);  // and here
 
 app.listen(port, () => console.log(`Server started listening to port ${port}`));
+
+
